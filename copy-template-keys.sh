90b392698cf660de1ebf665cc7cc8842c40831ed
boognu <boognu@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2007-08-13 19:21:26 +0000

    keyfile templates replaced

--- copy-template-keys.sh
+++ copy-template-keys.sh
@@ -1,3 +1,57 @@
 #!/bin/bash
-cp template.entropy "alexapfel@gmail.com simon.wenner@gmail.com 34EF4500.pad"
-cp template-rev.entropy "simon.wenner@gmail.com alexapfel@gmail.com 34EF4500.pad"
+# Creates copies of the keyfile templates from the SVN
+echo
+echo 
+echo "Please remember: The templates for the keys are from public SVN. You can use the program pad.py (branches/python-otp) to create keys."
+echo
+echo  Hit Enter to continue....
+read
+
+copyfiles() {
+
+for s in $SRC; do
+	for d in $DST; do
+            if [ $s = $d ]; then
+		#echo Source == Dest: I use the self template
+		cp loop.entropy "$DIR $s $d 11111111.pad"
+	    else
+		#echo Source <> Dest: I use the normal templates
+		cp template.entropy "$DIR$s $d 22222222.pad"
+		cp template-rev.entropy "$DIR$d $s 22222222.pad"
+            fi
+	#echo $s $d
+	done
+done
+
+}
+
+DIR="$HOME/.paranoia/"
+
+mkdir $DIR
+
+
+SRC="simon.wenner@gmail.com alexapfel@gmail.com alexapfel@swissjabber.ch nowic@swissjabber.ch"
+DST=$SRC
+
+copyfiles
+
+SRC="alice@jabber.org bob@jabber.org"
+DST=$SRC
+
+copyfiles
+
+SRC="76239710 112920906"
+DST=$SRC
+
+copyfiles
+
+SRC="fredibraatsmaal@hotmail.com"
+DST=$SRC
+
+copyfiles
+
+echo
+echo "Done! Your keys are stored in $DIR".
+echo
+
+
