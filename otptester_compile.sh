644379402295f1729c59d10f2f75aee4e57cf53b
boognu <boognu@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2007-08-19 23:52:49 +0000

    working with glib 2.12

--- otptester_compile.sh
+++ otptester_compile.sh
@@ -2,4 +2,4 @@
 # Help: http://www.network-theory.co.uk/docs/gccintro/gccintro_11.html
 # Help: http://developer.gnome.org/doc/API/2.0/glib/glib-compiling.html
 echo "gcc `pkg-config --cflags --libs glib-2.0` otptester.c libotp.c -o otptester"
-gcc `pkg-config --cflags --libs glib-2.0` otptester.c libotp.c -o otptester
+gcc -Wall `pkg-config --cflags --libs glib-2.0` otptester.c libotp.c -o otptester
