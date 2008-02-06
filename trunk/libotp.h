e66a11a1e984af02506a01c4800f858b573cea29
nowic <nowic@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2008-02-06 17:21:53 +0000

    merged the key_integration branche back into the trunk

--- trunk/libotp.h
+++ trunk/libotp.h
@@ -1,17 +1,17 @@
 /*
  * One-Time Pad Library - Encrypts strings with one-time pads.
  * Copyright (C) 2007-2008  Christian Wäckerlin
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
@@ -20,7 +20,7 @@
  * Changing this makes your one-time-pad incompatible */
 
 #define OTP_ID_LENGTH 8			/* Size of the ID-string. 4 bytes --> 8 bytes base 16*/
-#define OTP_PROTECTED_ENTROPY 100	/* The amount of entropy that is only used for "out of entropy" messages */ 
+#define OTP_PROTECTED_ENTROPY 100	/* The amount of entropy that is only used for "out of entropy" messages */
 
 /* ------------------ Error Syndrome System  ---------------------- */
 
@@ -59,7 +59,7 @@ typedef enum {
 	OTP_ERR_KEY_EMPTY	= 0x00030000,
 
 /* function: otp_decrypt
- * origin: otp_get_decryptkey_from_file 
+ * origin: otp_get_decryptkey_from_file
  * Position in the message does not exist in the entropy file */
 	OTP_ERR_KEY_SIZE_MISMATCH	 = 0x00040000,
 
@@ -68,7 +68,7 @@ typedef enum {
  * The input into the fuction is not valid i.e. NULL*/
 	OTP_ERR_INPUT				= 0x00050000,
 
-/* function: otp_decrypt 
+/* function: otp_decrypt
  * origin: otp_decrypt
  * The message is not in the format "3234|34EF4588|M+Rla2w=" and can not be splitted */
 	OTP_ERR_MSG_FORMAT			= 0x00060000,
@@ -120,21 +120,21 @@ struct otp;
 
 /* ------------------ Principal functions ----------------------------*/ 
 
-/* encrypt the message 
+/* encrypt the message
  * if it can't encrypt the message a syndrome > OTP_WARN is returned and
  * the message is left unchanged */
 OtpError otp_encrypt(struct otp* mypad, char** message);
 
-/* encrypts a message with the protected entropy. protected_pos is the position in bytes to use. 
- * The entropy is not consumed by this function. 
- * To used the function securely, the signal-messages should not overlap 
- * and every signal has to stay constant! 
- * When only one signal is used, use protected_pos = 0. 
+/* encrypts a message with the protected entropy. protected_pos is the position in bytes to use.
+ * The entropy is not consumed by this function.
+ * To used the function securely, the signal-messages should not overlap
+ * and every signal has to stay constant!
+ * When only one signal is used, use protected_pos = 0.
  * if it can't encrypt the message a syndrome > OTP_WARN is returned and
  * the message is left unchanged */
 OtpError otp_encrypt_warning(struct otp* mypad, char** message, gsize protected_pos);
 
-/* decrypt the message 
+/* decrypt the message
  * if it can't decrypt the message a syndrome > OTP_WARN is returned and
  * the message is left unchanged */
 OtpError otp_decrypt(struct otp* mypad, char** message);
@@ -200,26 +200,26 @@ struct otp_config* otp_pad_get_conf(struct otp* mypad);
 
 /* ------------------ otp_config ------------------------------ */
 
-/* Creation of the config stucture of the library, sets some parameters 
- * 
- * client_id:		The name of the application using the library, i.e. 'paranoia-core' 
+/* Creation of the config stucture of the library, sets some parameters
+ *
+ * client_id:		The name of the application using the library, i.e. 'paranoia-core'
  * path:		The path where the .entropy-files are stored.
- * export_path:		The path where to export new created keys 
+ * export_path:		The path where to export new created keys
  * 					for the other converstation partner i.e. 'bob' */
-struct otp_config* otp_conf_create(const char* client_id, 
+struct otp_config* otp_conf_create(const char* client_id,
 				const char* path, const char* export_path);
 
-/* Freeing of the otp_config struct 
+/* Freeing of the otp_config struct
  * This fails with OTP_ERR_CONFIG_PAD_COUNT if there are any pads open in this config */
 OtpError otp_conf_destroy(struct otp_config* myconfig);
 
 /* ------------------ otp_config get functions ------------------- */
 
-/* Gets a reference to the path in the config 
+/* Gets a reference to the path in the config
  * Does not need to be freed.  */
 const char* otp_conf_get_path(const struct otp_config* myconfig);
 
-/* Gets a reference to the export path in the config 
+/* Gets a reference to the export path in the config
  * Does not need to be freed.  */
 const char* otp_conf_get_export_path(const struct otp_config* myconfig);
 
@@ -238,17 +238,36 @@ OtpError otp_conf_set_path(struct otp_config* myconfig, const char* path);
 OtpError otp_conf_set_export_path(struct otp_config* myconfig, const char* export_path);
 
 /* Sets random_msg_tail_max_len:
- * 					The max length of the randomly added tailing charakters 
+ * 					The max length of the randomly added tailing charakters
  * 					to prevent 'eve' from knowng the length of the message.
  * 					Disabled if 0. Default is already set to DEFAULT_RNDLENMAX */
 OtpError otp_conf_set_random_msg_tail_max_len(struct otp_config* myconfig,
 				 gsize random_msg_tail_max_len);
 
-/* Sets msg_key_improbability_limit: 
+/* Sets msg_key_improbability_limit:
  * 					If the used random entropy shows pattern that are less likely
- * 					then this limit, the entropy is discarded and an other block of 
+ * 					then this limit, the entropy is discarded and an other block of
  * 					entropy is used. A warning OTP_WARN_KEY_NOT_RANDOM is given.
  * 					Disabled if 0.0. Default is already set to DEFAULT_IMPROBABILITY. */
 OtpError otp_conf_set_msg_key_improbability_limit(struct otp_config* myconfig,
 				 double msg_key_improbability_limit);
 
+/* Generate a key-pair alice and bob with size size */
+GThread *generate_keys_from_keygen(char *alice, char *bob, unsigned int size, int loop);
+
+/* get a random id for the key filename */
+unsigned int otp_get_id();
+
+GThread *generate_keys_from_file(char *alice, char *bob, char *file, unsigned int size, int loop);
+
+/* invert:
+*				invert writes the bytewise inverse of the src file into the dest
+*				file. src and dest must be valid file names
+*				this function returns 0 for success, -1 if a failure occures. */
+//int otp_invert(char *src, char *dest);
+
+/* loop-invert:
+*				append the bytewise inverse of src to src
+*				src must be a valide filename with valide path
+*				returns 0 for success, -1 if a failure occures */
+//int otp_loop_invert(char *src);
