f746069a44a0abbf13157688917f035b7dd959f3
nowic <nowic@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2008-02-06 17:39:13 +0000

    minor cleanup

--- libotp.h
+++ libotp.h
@@ -1,6 +1,6 @@
 /*
  * One-Time Pad Library - Encrypts strings with one-time pads.
- * Copyright (C) 2007-2008  Christian Wäckerlin
+ * Copyright (C) 2007-2008  Christian Wäckerlin, Simon Wener
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
