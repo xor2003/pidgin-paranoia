d0d34bc904add4c7ed00dd7ca6526d2e11c79278
edrundor <edrundor@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2008-02-14 12:15:05 +0000

    only 1 key generation at a time possible, maybe still have some bugs

--- trunk/keygen.h
+++ trunk/keygen.h
@@ -16,20 +16,19 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * */
 
-
 /* Generate a key-pair alice and bob with size size */
 GThread *keygen_keys_generate(char *alice_file, char *bob_file,
-		gsize size, gboolean is_loopkey);
+		gsize size, gboolean is_loopkey, void* config);
 
 /* Generate a key-pair alice and bob with size size out of the entropy in file */
 GThread *keygen_keys_generate_from_file(const char *alice_file,
 		const char *bob_file, const char *entropy_src_file,
-		gsize size, gboolean is_loopkey);
+		gsize size, gboolean is_loopkey, void* config);
 
 /* Generate a key-pair alice and bob with size size out of a character device */		
 GThread *keygen_keys_generate_from_device(const char *alice_file, 
 		const char *bob_file, const char *device, gsize size, 
-		gboolean is_loopkey);
+		gboolean is_loopkey, void* config);
 
 /* get a random id for the key filename */
 unsigned int keygen_id_get();		
