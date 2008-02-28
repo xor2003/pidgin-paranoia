2a9971a584220497b89d0b5c662c86c4b42b9f20
edrundor <edrundor@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2008-02-28 10:57:30 +0000

    temp keys

--- trunk/libotp.c
+++ trunk/libotp.c
@@ -564,10 +564,10 @@ OtpError otp_generate_key_pair(struct otp_config *config,
 	/* create filenames with the correct path*/
 	id = keygen_id_get();
 	
-	alice_file = (char *)g_strdup_printf("%s%s%s%s%s%s%.8X.entropy", 
+	alice_file = (char *)g_strdup_printf("%s%s%s%s%s%s%.8X", 
 			otp_conf_get_path(config), 
 			PATH_DELI, alice, FILE_DELI, bob, FILE_DELI, id);
-	bob_file = (char *)g_strdup_printf("%s%s%s%s%s%s%.8X.entropy", 
+	bob_file = (char *)g_strdup_printf("%s%s%s%s%s%s%.8X", 
 			otp_conf_get_export_path(config), 
 			PATH_DELI, bob, FILE_DELI, alice, FILE_DELI, id);
 	
