46cc9213b0f951b3d47797b625cb77b0fa4649b3
nowic <nowic@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2008-12-21 22:29:33 +0000

    gettext support added, some strings are buggy

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -4,9 +4,17 @@
 # 1. delete all auto generated files (or get a fresh working copy from svn)
 # 2. run this script
 
+(intltoolize --version) < /dev/null > /dev/null 2>&1 || {
+    echo;
+    echo "You must have intltool installed to compile pidgin-paranoia";
+    echo;
+    exit;
+}
+intltoolize --force --copy
+echo "intltoolize done!"
 aclocal
 echo "aclocal done!"
-libtoolize --copy
+libtoolize --force --copy
 echo "libtoolize done!"
 autoheader
 echo "autoheader done!"
@@ -14,3 +22,4 @@ automake --add-missing --copy
 echo "automake done!"
 autoconf
 echo "autoconf done!"
+
