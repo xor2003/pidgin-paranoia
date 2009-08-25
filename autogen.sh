991bde4ac6c3461e30a5a06fc857f2ee2303c416
nowic <nowic@56c7e8e3-7a36-0410-81cd-cf4cb6bd70c8>
2009-08-25 16:31:43 +0000

    Cleanup build system, update changelog

--- autogen.sh
+++ autogen.sh
@@ -4,22 +4,22 @@
 # 1. delete all auto generated files (or get a fresh working copy from svn)
 # 2. run this script
 
+automake --add-missing --copy
+echo "automake done!"
 (intltoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo;
     echo "You must have intltool installed to compile pidgin-paranoia";
     echo;
     exit;
 }
-intltoolize --force --copy
+intltoolize --force --copy --automake
 echo "intltoolize done!"
-aclocal
-echo "aclocal done!"
 libtoolize --force --copy
 echo "libtoolize done!"
 autoheader
 echo "autoheader done!"
-automake --add-missing --copy
-echo "automake done!"
+aclocal
+echo "aclocal done!"
 autoconf
 echo "autoconf done!"
 
